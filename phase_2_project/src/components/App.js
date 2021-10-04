b5ed9592d9c2547c44ea07eae2188c216c2dbf18
Pasha <pashdoctor@gmail.com>
2021-10-04 17:51:25 -0400

    added stuff

--- phase_2_project/src/components/App.js
+++ phase_2_project/src/components/App.js
@@ -1,7 +1,15 @@
+import { useState, useEffect } from "react";
 import AccountBalance from "./AccountBalance";
 import StatementContainer from "./StatementContainer";
 
 function App() {
+  const [accountData, setAccountData] = useState([])
+
+  useEffect(() => {
+    fetch('http://localhost:4000/Account')
+    .then(resp => resp.json())
+    .then(data => setAccountData(data))
+  },[])
   return (
     <div>
       <AccountBalance/>
