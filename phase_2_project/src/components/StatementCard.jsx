f9b7f3cc99abe328071376e3d50fb52d992e876a
Gerald <ggaereminck@yahoo.com>
2021-10-06 15:54:12 -0400

    added hide transaction buttons

--- phase_2_project/src/components/StatementCard.jsx
+++ phase_2_project/src/components/StatementCard.jsx
@@ -1,13 +1,21 @@
 import React from 'react'
+import { useState } from 'react'
 
 export default function StatementCard({trans}) {
+    const [hideTransaction, setHideTransaction] = useState(true)
+
     return(
     <div>
-        <h2>{trans.type}</h2>
-        <h3>{trans.amount}</h3>
-        <p>{trans.userNotes}</p>
-        <p>{trans.date}</p>
-        <p>{trans.currency}</p>
-        <p>{trans.accountNumber}</p>
+        <button onClick={() => setHideTransaction(!hideTransaction)}>{hideTransaction ? "Minimize Transaction" : "Show Transaction"}</button>
+            {hideTransaction ? 
+            <div>
+                <h2>{trans.type}</h2>
+                <h3>{trans.amount}</h3>
+                <p>{trans.userNotes}</p>
+                <p>{trans.date}</p>
+                <p>{trans.currency}</p>
+                <p>{trans.accountNumber}</p> 
+            </div>
+            : null }
     </div>)
     }
